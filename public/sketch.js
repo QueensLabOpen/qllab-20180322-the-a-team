d400865ef9b45f26aef8a614e0905b8d7612b72c
John Petersson <johnpetersson@gmail.com>
2018-03-22 18:37:08 +0100

    Initial

---
+++ public/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // put setup code here
+  createCanvas(800, 600);
+}
+
+function draw() {
+  // put drawing code here
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 30, 30);
+}
