2a28464ba12dd4c7810a0dbd968ab42f67d6f712
John Petersson <johnpetersson@gmail.com>
2018-03-22 20:24:01 +0100

    Input clean step 1

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,30 @@
+
+class Queue {
+  constructor() {
+    this.content = [];
+    this.buffer = '';
+  }
+
+  add (input) {
+    this.buffer += input;
+    console.log(this.buffer);
+    if (this.buffer.includes(';')) {
+      this.content.push(this.buffer.trim());
+      this.buffer = '';
+    } else {
+      // Do nothing, wait for more input
+    }
+  }
+
+  getFirst() {
+    return this.content.shift();
+  }
+
+
+}
+
+let inputQueue = new Queue();
+
 // Create a new WebSocket.
 var socket = new WebSocket('ws://localhost:40510');
 
@@ -6,8 +33,16 @@ socket.onopen = function(event) {
 }
 
 socket.onmessage = function(event) {
-  console.log(event.data);
+  // console.log(event.data);
+  inputQueue.add(event.data);
 }
+
+// let buffer;
+// clean() = function(input) {
+//
+// }
+
+let latestInput = '';
 
 function setup() {
   // put setup code here
@@ -16,5 +51,14 @@ function setup() {
 
 function draw() {
   // put drawing code here
+  background('white');
 
+  let temp = inputQueue.getFirst();
+  if (temp) {
+    latestInput = temp;
+    console.log(latestInput);
+  }
+  textSize(32);
+  text(latestInput, 10, 30);
+  fill(0, 102, 153);
 }
